6d38fa084a6707803eb5548f8dfe503f19de38cf
Sam Zaccone <samrzaccone@gmail.com>
2026-01-26 10:20:51 -0500

    set up main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:information_dam/features/authentication/auth_controller.dart';
+import 'package:information_dam/features/authentication/auth_repository.dart';
 
-void main() {
-  runApp(const App());
+final kTheme = ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF006633)));
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ProviderScope(child: const App()));
 }
 
-class App extends StatelessWidget {
+class App extends ConsumerStatefulWidget {
   const App({super.key});
 
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _AppState();
+}
+
+class _AppState extends ConsumerState<App> {
+  void _getData(User data) async {
+    final person = await ref.read(authRepositoryProvider).getPersonData(data.uid).first;
+    ref.read(personProvider.notifier).update((state) => person);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
-      home: const Placeholder(),
+      debugShowCheckedModeBanner: false,
+      title: "Information Dam",
+        theme: kTheme, 
+        home: Placeholder(),
     );
   }
 }
